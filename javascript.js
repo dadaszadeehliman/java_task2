956a74cee15e44ea6c318c632d9d9f335b54dbb1
dadaszadeehliman <ld7whnra9@code.edu.az>
2024-04-25 13:58:12 +0400

    hazir

--- javascript.js
+++ javascript.js
@@ -7,14 +7,14 @@ function bank(emekHaqqi, istediyinizMebleg, istediyinizMuddet) {
     let f = (((istediyinizMebleg * 17) / 100) + istediyinizMebleg) / istediyinizMuddet;
     let x = ((emekHaqqi * 45) / 100)
     if (y > x || z > x || f > x) {
-        return "sehv"
-    }
-    if (istediyinizMuddet <= 12) {
-        return istediyinizMebleg + " " + "azn mebleg size 13%le" + " " + istediyinizMuddet + " " + "ay muddetinde ayliq" + " " + y + " " + "manat odenisle verilir"
+        return "Size bu sertle kridet dusmur!"
+    } else if (istediyinizMuddet <= 12) {
+        return istediyinizMebleg + " " + "azn mebleg size 13%le" + " " + istediyinizMuddet + " " + "ay muddetinde ayliq" + " " + Math.round(y) + " " + "manat odenisle verilir"
     } else if (istediyinizMuddet > 12 && istediyinizMuddet <= 24) {
-        return istediyinizMebleg + " " + "azn mebleg size 15%le" + " " + istediyinizMuddet + " " + "ay muddetinde ayliq" + " " + z + " " + "manat odenisle verilir"
+        return istediyinizMebleg + " " + "azn mebleg size 15%le" + " " + istediyinizMuddet + " " + "ay muddetinde ayliq" + " " + Math.round(z) + " " + "manat odenisle verilir"
     } else if (istediyinizMuddet > 24 && istediyinizMuddet <= 36) {
-        return istediyinizMebleg + " " + "azn mebleg size 17%le" + " " + istediyinizMuddet + " " + "ay muddetinde ayliq" + " " + f + " " + "manat odenisle verilir"
+        return istediyinizMebleg + " " + "azn mebleg size 17%le" + " " + istediyinizMuddet + " " + "ay muddetinde ayliq" + " " + Math.round(f) + " " + "manat odenisle verilir"
     }
 }
-console.log(bank(1800, 10000, 13)); 
+let qiymet =bank(5000, 2500, 36) 
+console.log(qiymet);
